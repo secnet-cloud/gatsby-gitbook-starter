16673478a05bb7e3abebb28288fffefe353c0410
secnet-cloud <80630867+secnet-cloud@users.noreply.github.com>
2021-03-20 19:51:50 +0000

    41

--- config.js
+++ config.js
@@ -29,11 +29,11 @@ const config = {
 		  </li>`,
     links: [{ text: '', link: '' }],
     search: {
-      enabled: false,
-      indexName: '',
-      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
-      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
+      "enabled": true,
+  		"indexName": "MY_INDEX_NAME",
+  		"algoliaAppId": process.env.GATSBY_ALGOLIA_APP_ID,
+  		"algoliaSearchKey": process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+  		"algoliaAdminKey": process.env.ALGOLIA_ADMIN_KEY
     },
   },
   sidebar: {
